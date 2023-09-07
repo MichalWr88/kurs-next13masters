2f6c81dad70ae841e6c10c5cd352dc58a6c1d610
mmaleszewski <mmaleszewski@trans.eu>
2023-09-07 14:57:07 +0200

    chore: change main layout

---
+++ src/UI/Body/Body.tsx
@@ -0,0 +1,15 @@
+import React, { type ReactNode } from "react";
+import  "./Body.scss";
+type Props = {
+	children: ReactNode;
+};
+
+const Body = ({ children }: Props) => (
+	<body
+		className={`flex h-screen w-screen flex-col content-center items-center  globalBg`}
+	>
+		{children}
+	</body>
+);
+
+export default Body;
--- src/app/(withNavBar)/layout.tsx
+++ src/app/(withNavBar)/layout.tsx
@@ -1,9 +1,9 @@
-import Footer from "@/app/UI/Footer/Footer";
-import NavBar from "@/app/UI/NavBar/NavBar";
+import Footer from "@/UI/Footer/Footer";
+import NavBar from "@/UI/NavBar/NavBar";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="flex h-full w-full flex-col relative overflow-auto">
+		<div className="flex h-full w-full flex-col relative overflow-auto ">
 			<NavBar />
 			<main className="flex-grow">{children}</main>
 			<Footer />
