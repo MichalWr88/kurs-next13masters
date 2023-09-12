95b4174467d79b667dc6d10df778690bf4592d33
mmaleszewski <mmaleszewski@trans.eu>
2023-09-12 19:55:34 +0200

    feat: add about, categories, category page

---
+++ src/app/(withNavBar)/about/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+
+const PageAbout = () => (
+    <div>PageAbout</div>
+  )
+
+export default PageAbout
---
+++ src/app/(withNavBar)/categories/[categoryId]/page.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+
+
+const CategoriesIdPage = () => (
+    <div>category</div>
+  )
+
+export default CategoriesIdPage
