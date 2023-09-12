face459c04d430617b20b02671152af1aec3e1c9
mmaleszewski <mmaleszewski@trans.eu>
2023-09-12 19:50:33 +0200

    chore: add typedRoutes and redirect products

--- next.config.js
+++ next.config.js
@@ -1,7 +1,19 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+	async redirects() {
+    return [
+      {
+        source: '/products',
+        destination: '/products/1',
+        permanent: true,
+      },
+    ]
+  },
+	experimental: {
+    typedRoutes: true,
+  },
 	images: {
-		domains: ["fakestoreapi.com"],
+		domains: ["fakestoreapi.com","naszsklep-api.vercel.app"],
 	},
 };
 
