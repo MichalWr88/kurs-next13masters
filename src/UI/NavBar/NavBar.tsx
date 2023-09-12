c252856b893d47b2bd369c8c4db67f95b512d1ba
mmaleszewski <mmaleszewski@trans.eu>
2023-09-12 19:58:15 +0200

    feat: update nav bar  for scroll ref

--- src/UI/NavBar/NavBar.tsx
+++ src/UI/NavBar/NavBar.tsx
@@ -1,14 +1,21 @@
-
+"use client";
+import { clsx } from "clsx";
+import { forwardRef, useMemo } from "react";
 import NavList from "../Navigation/NavList";
+type Props = {
+	hide: boolean;
+};
+// eslint-disable-next-line react/display-name
+const NavBar = forwardRef<HTMLDivElement, Props>(({ hide }, ref) => {
+	const navList = useMemo(() => <NavList />, []);
 
-const NavBar = async () => (
-	<header className="bg-brand-color-3 sticky top-0">
-		<nav className="flex justify-center p-4">
-			<ul className="container flex h-8">
-				<NavList />
-			</ul>
-		</nav>
-	</header>
-);
+	return (
+		<header className={clsx("sticky top-0 z-50 bg-brand-color-3 transition-all ease-in-out duration-500 delay-150", hide && "-top-28")} ref={ref}>
+			<nav className="flex justify-center p-4">
+				<ul className="container flex h-8">{navList}</ul>
+			</nav>
+		</header>
+	);
+});
 
 export default NavBar;
