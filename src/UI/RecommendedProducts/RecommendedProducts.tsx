f209f1c05185414c9a8af23d1b1fe89a922e8c6d
mmaleszewski <mmaleszewski@trans.eu>
2023-09-18 20:10:46 +0200

    chore: update recommended protucts

--- src/UI/RecommendedProducts/RecommendedProducts.tsx
+++ src/UI/RecommendedProducts/RecommendedProducts.tsx
@@ -1,56 +1,30 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import { executeGraphql } from "@/api/graphQL/graphQLProvider";
+import { ProductsGetListDocument } from "@/gql/graphql";
 import { getProductsList } from "@/providers/restApiProvider/restApiProvider";
+import ProductImg from "../Product/ProductImg";
 
 const RecommendedProducts = async () => {
-	const products = await getProductsList({ take: "5", offset: "100" });
-
+	// const products = await getProductsList({ take: "5", offset: "100" });
+	const resp = await executeGraphql(ProductsGetListDocument, {
+		page: 1,
+		pageSize: 4,
+		sort: ["rating:DESC"],
+	});
+	const products = resp.products?.data;
 	return (
-		<section>
+		<section className="p-2">
 			<header>
-				<h1>Recommended Products</h1>
+				<h2 className="text-xl font-bold">Recommended Products</h2>
 			</header>
-			<ul className="flex bg-brand-color-3">
-				{products.map((item) => {
-					const { id, category, image, title, price, rating } = item;
-					return (
-						<li key={`recommended-${id}`}>
-							<figure className="">
-								<Link
-									className="relative flex h-52 w-full items-center justify-center overflow-hidden rounded-3xl"
-									href={`/product/${id}`}
-								>
-									<Image
-										className="relative "
-										quality={20}
-										priority
-										data-testid="mePic"
-										src={image}
-										alt={title}
-										width={200}
-										height={200}
-										style={{ objectFit: "cover" }}
-									/>
-								</Link>
-								<figcaption className="row">
-									<div className="row justify-between">
-										<h3 className="p-2 text-xl font-bold">{title}</h3>
-										<div className="">
-											<b>$</b>
-											{price}
-										</div>
-										<div>{rating.count}</div>
-										<div>{rating.rate}</div>
-									</div>
-									<div className="row justify-between p-2">
-										<div>{category} </div>
-									</div>
-								</figcaption>
-							</figure>
+			<ul className="grid grid-cols-1 gap-8 p-4 lg:grid-cols-4">
+				{products?.map((product) => (
+						<li key={`recommended-${product.attributes?.slug}`}>
+							<ProductImg product={product.attributes} />
 						</li>
-					);
-				})}
+					))}
 			</ul>
 		</section>
 	);
