65b01907b6ca47ad3a11c966b2089f81658e2b7c
mmaleszewski <mmaleszewski@trans.eu>
2023-09-18 06:24:29 +0200

    feat:  refactor product list

--- src/app/(withNavBar)/products/[pageTakeNumber]/layout.tsx
+++ src/app/(withNavBar)/products/[pageTakeNumber]/layout.tsx
@@ -1,30 +1,22 @@
 import { Suspense } from "react";
 import RecommendedProducts from "@/UI/RecommendedProducts/RecommendedProducts";
 import RoutePagination from "@/UI/RoutePagination/RoutePagination";
-import { getProductsList } from "@/providers/restApiProvider/restApiProvider";
-import { prodOnly } from "@/utils";
+import { executeGraphql } from "@/api/graphQL/graphQLProvider";
+import { ProductsGetCountListDocument } from "@/gql/graphql";
 
-const getPaginationTotalCount = async () => {
-	const take = 1000;
-	let offset = 0;
-	let total = 0;
-	let respons = -1;
-
-	while (respons !== 0) {
-		const resp = await getProductsList({ take: take.toString(), offset: offset.toString() });
-		respons = resp.length;
-		offset += take + 1;
-		total += respons;
-	}
-	return total;
-};
 
 export default async function RootLayout({ children }: { children: React.ReactNode }) {
-	const totalCount = await prodOnly(getPaginationTotalCount(), 100);
+	const resp = await executeGraphql(ProductsGetCountListDocument, {});
 
+	console.log("total",resp.products?.meta.pagination.total)
 	return (
 		<div className="container mb-3">
-			<RoutePagination basePath="/products" currentPage={1} totalCount={totalCount} pageSize={20} />
+			<RoutePagination
+				basePath="/products"
+				currentPage={1}
+				totalCount={resp.products?.meta.pagination.total?? 0}
+				pageSize={20}
+			/>
 			{children}
 			<Suspense fallback={"loading...."}>
 				<RecommendedProducts />
--- src/app/(withNavBar)/products/[pageTakeNumber]/page.tsx
+++ src/app/(withNavBar)/products/[pageTakeNumber]/page.tsx
@@ -1,8 +1,10 @@
 import Image from "next/image";
 import Link from "next/link";
 import React from "react";
+import Rating from "@/UI/Rating/Rating";
+import { executeGraphql } from "@/api/graphQL/graphQLProvider";
+import { ProductsGetListDocument } from "@/gql/graphql";
 import { getProductsList } from "@/providers/restApiProvider/restApiProvider";
-import "./ProductPage.scss";
 export const generateStaticParams = async () => [
 	{ pageTakeNumber: "1" },
 	{ pageTakeNumber: "2" },
@@ -14,48 +16,69 @@ const ProductPage = async ({
 }: {
 	params: { pageTakeNumber: string };
 }) => {
-	const products = await getProductsList({ offset: (20*(Number(pageTakeNumber)-1)).toString(), take: "20" });
+	const resp = await executeGraphql(ProductsGetListDocument, {
+		page: Number(pageTakeNumber),
+		pageSize: 20,
+	});
 
+	console.log(resp.products);
 	return (
-		<>
-			<h1 className="bg-brand-bg-color uppercase p-2 text-xl container">Products list</h1>
-			<ul data-testid="products-list" className="grid grid-cols-1 gap-8 p-2 md:grid-cols-4">
-				{products.map((item) => {
-					const { id, description, category, image, title, price, rating } = item;
-					return (
-						<li key={`product-${id}`} className="flex  justify-center bg-white rounded-3xl">
-							<figure className="">
-								<Link className="imgWrapper  relative flex h-52 w-full items-center justify-center overflow-hidden rounded-3xl" href={`/product/${id}`}>
-									<Image
-										className="relative h-max transition-all delay-100 duration-300 ease-in-out "
-										quality={20}
-										priority
-										data-testid="mePic"
-										src={image}
-										alt={title}
-										width={200}
-										height={200}
-										style={{ objectFit: "cover" }}
-									/>
-								</Link>
-								<figcaption className="row">
-									<div className="row justify-between">
-										<h3 className="p-2 text-xl font-bold">{title}</h3>
-										<div className=""><b>$</b>{price}</div>
-										<div>{rating.count}</div>
-										<div>{rating.rate}</div>
-									</div>
-									<div className="row justify-between p-2">
-										<div>{category} </div>
-										<div>{description}</div>
-									</div>
-								</figcaption>
-							</figure>
-						</li>
-					);
-				})}
-			</ul>
-		</>
+		<section className="body-font text-gray-600">
+			<header>
+				<h1 className="container bg-brand-bg-color p-2 text-xl uppercase">Products list</h1>
+			</header>
+			<div className="container mx-auto px-5 py-24">
+				<ul data-testid="products-list" className="grid grid-cols-1 gap-8 p-2 lg:grid-cols-4 sm:grid-cols-3">
+					{resp.products?.data.map((item) => {
+						if (!item.attributes) return null;
+						const { slug, title, description, price, categories, rating, images } = item.attributes;
+
+						return (
+							<li key={`product-${slug}`} className="">
+								<figure className="">
+									{images?.data[0] && (
+										<Link
+											className="relative block h-56 w-full items-center justify-center overflow-hidden rounded-3xl"
+											href={`/product/${slug}`}
+										>
+											<Image
+												className="relative transition-all delay-100 duration-300 ease-in-out  "
+												quality={20}
+												priority
+												data-testid="mePic"
+												src={`${process.env.GRAPHQL_URL}${images?.data[0].attributes?.url}` || ""}
+												alt={title}
+												// width={images?.data[0].attributes?.width ?? 200}
+												// height={images?.data[0].attributes?.height ?? 200}
+												fill
+												style={{ objectFit: "fill" }}
+											/>
+										</Link>
+									)}
+									<figcaption className="mt-4">
+										{categories &&
+											categories.data?.map((cat) => (
+												<h3
+													key={`categories-${cat.attributes?.slug}`}
+													className="title-font mb-1 text-xs tracking-widest text-gray-500"
+												>
+													{cat.attributes?.name}
+												</h3>
+											))}
+
+										<h2 className="title-font text-lg font-medium text-gray-900">Shooting Stars</h2>
+										<div className="flex justify-between">
+											<Rating rating={rating}/>
+											<p className="mt-1">$ {price}</p>
+										</div>
+									</figcaption>
+								</figure>
+							</li>
+						);
+					})}
+				</ul>
+			</div>
+		</section>
 	);
 };
 
