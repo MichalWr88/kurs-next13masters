b2a62bb13c109c14fddf6a008b1a25cf437d1947
mmaleszewski <mmaleszewski@trans.eu>
2023-09-18 06:24:45 +0200

    feat: add graphQl Provider

---
+++ src/api/graphQL/graphQLProvider.ts
@@ -0,0 +1,43 @@
+import { type TypedDocumentString } from "@/gql/graphql";
+
+type GraphQLResponse<T> =
+	| { data?: undefined; errors: { message: string }[] }
+	| { data: T; errors?: undefined };
+
+ 
+  
+  export const executeGraphql = async <TResult, TVariables>(
+    query: TypedDocumentString<TResult, TVariables>,
+    variables: TVariables,
+  ): Promise<TResult> => {
+    if (!process.env.GRAPHQL_URL) {
+      throw TypeError("GRAPHQL_URL is not defined");
+    }
+    if (!process.env.GRAPHQL_TOKEN) {
+      throw TypeError("GRAPHQL_TOKEN is not defined");
+    }
+   
+    const res = await fetch(`${process.env.GRAPHQL_URL}/graphql`, {
+      method: "POST",
+      body: JSON.stringify({
+        query,
+        variables,
+      }),
+      headers: {
+        "Authorization":`Bearer ${process.env.GRAPHQL_TOKEN}`,
+        "Content-Type": "application/json",
+      },
+    });
+   
+    const graphqlResponse = (await res.json()) as GraphQLResponse<TResult>;
+   
+    if (graphqlResponse.errors) {
+      console.log(graphqlResponse.errors)
+      throw TypeError(`GraphQL Error`, {
+        cause: graphqlResponse.errors,
+      });
+   
+    }
+   
+    return graphqlResponse.data;
+  };
