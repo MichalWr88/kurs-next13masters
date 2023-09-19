06c86b4bb8ceb2a9e62efaef1e4084262f1e9428
mmaleszewski <mmaleszewski@trans.eu>
2023-09-19 14:42:43 +0200

    chore: add prod url for images

--- next.config.js
+++ next.config.js
@@ -1,24 +1,29 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	async redirects() {
-    return [
-      {
-        source: '/products',
-        destination: '/products/1',
-        permanent: true,
-      },
-      {
-        source: '/categories/:slug',
-        destination: '/categories/:slug/1', // Matched parameters can be used in the destination
-        permanent: true,
-      },
-    ]
-  },
+		return [
+			{
+				source: "/products",
+				destination: "/products/1",
+				permanent: true,
+			},
+			{
+				source: "/categories/:slug",
+				destination: "/categories/:slug/1", // Matched parameters can be used in the destination
+				permanent: true,
+			},
+		];
+	},
 	experimental: {
-    typedRoutes: true,
-  },
+		typedRoutes: true,
+	},
 	images: {
-		domains: ["fakestoreapi.com","naszsklep-api.vercel.app","localhost"],
+		domains: [
+			"fakestoreapi.com",
+			"naszsklep-api.vercel.app",
+			"localhost",
+			"kurs-next13masters.vercel.app",
+		],
 	},
 };
 
