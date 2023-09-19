9fcb17dbb2e9b11e9fd33468fd873c3ba0e6cda9
mmaleszewski <mmaleszewski@trans.eu>
2023-09-19 13:45:52 +0200

    feat: add appLink

---
+++ src/UI/Shared/AppLink.tsx
@@ -0,0 +1,19 @@
+"use client";
+import clsx from "clsx";
+import { type Route } from "next";
+import Link from "next/link";
+
+import React, { type ReactNode } from "react";
+
+type Props<T extends string> = {
+	children: ReactNode;
+	route: Route<T>;
+};
+
+const AppLink = <T extends string>({ children, route }: Props<T>) => (
+	<Link className={clsx("relative hover:font-bold")} href={route}>
+		{children}
+	</Link>
+);
+
+export default AppLink;
