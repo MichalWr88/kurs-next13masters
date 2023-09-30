a149e2780e59246fdde2ff81d01f108557dfdcfd
mmaleszewski <mmaleszewski@trans.eu>
2023-09-30 08:38:58 +0200

    feat: addToCard

---
+++ src/UI/Buttons/AddCartButton.tsx
@@ -0,0 +1,18 @@
+"use client"
+import { experimental_useFormStatus as useFormStatus } from "react-dom";
+
+const AddCartButton = () => {
+	const formStatus = useFormStatus();
+
+	return (
+		<button
+			disabled={formStatus.pending}
+			type="submit"
+			className="ml-auto flex rounded border-0 bg-indigo-500 px-6 py-2 text-white hover:bg-indigo-600 focus:outline-none disabled:cursor-wait disabled:bg-zinc-400"
+		>
+			Add to cart
+		</button>
+	);
+};
+
+export default AddCartButton;
--- src/app/(withNavBar)/product/[slugId]/page.tsx
+++ src/app/(withNavBar)/product/[slugId]/page.tsx
@@ -1,19 +1,38 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
+
+import { revalidatePath } from "next/cache";
+import { cookies } from "next/headers";
 import { notFound } from "next/navigation";
+import AddCartButton from "@/UI/Buttons/AddCartButton";
 import ProductImg from "@/UI/Product/ProductImg";
-import { executeGraphql } from "@/api/graphQL/graphQLProvider";
+import { getOrCreateCart } from "@/api/cartProider";
+import { createCartItem, executeGraphql } from "@/api/graphQL/graphQLProvider";
 import { ProductGetBySlugDocument } from "@/gql/graphql";
 
-
-
-// export const generateStaticParams = async () => {
-// 	const products = await prodOnly(getProductsList({ take: "50" }),[]);
-// 	return products.map((prod) => prod.id);
-// };
-
 const PageProduct = async ({ params: { slugId } }: { params: { slugId: string } }) => {
-	const resp = await executeGraphql(ProductGetBySlugDocument, { slug: slugId });
+	const resp = await executeGraphql({
+		query: ProductGetBySlugDocument,
+		variables: { slug: slugId },
+	});
 	const product = resp.products?.data[0]?.attributes;
-	if (!product) return notFound();
+	const id = resp.products?.data[0]?.id;
+	if (!product || !id) return notFound();
+
+	async function addProductToCartAction() {
+		"use server";
+
+		const cart = await getOrCreateCart();
+		if (!cart.attributes?.slug || !cart.id) return;
+		cookies().set("cartId", cart.attributes?.slug, {
+			httpOnly: true,
+			sameSite: "lax",
+			expires: new Date(Date.now() + 1000 * 60 * 60 * 24 * 7),
+		});
+
+		await createCartItem(Number(cart.id), Number(id), product?.price || 0);
+		revalidatePath("/cart"); // in my case it is not needed to revalidate
+	}
+
 	return (
 		<section className="min-h-screen">
 			<div className="container mx-auto px-5 py-24">
@@ -30,49 +49,8 @@ const PageProduct = async ({ params: { slugId } }: { params: { slugId: string } 
 						))}
 
 						<h1 className="title-font mb-1 text-3xl font-medium text-gray-900">{product.title}</h1>
-						<div className="mb-4 flex">
-							{/* <span className="flex items-center">
-            <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="currentColor" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <svg fill="none" stroke="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-4 h-4 text-indigo-500" viewBox="0 0 24 24">
-              <path d="M12 2l3.09 6.26L22 9.27l-5 4.87 1.18 6.88L12 17.77l-6.18 3.25L7 14.14 2 9.27l6.91-1.01L12 2z"></path>
-            </svg>
-            <span className="text-gray-600 ml-3">4 Reviews</span>
-          </span> */}
-							{/* <span className="flex ml-3 pl-3 py-2 border-l-2 border-gray-200 space-x-2s">
-            <a className="text-gray-500">
-              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"></path>
-              </svg>
-            </a>
-            <a className="text-gray-500">
-              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"></path>
-              </svg>
-            </a>
-            <a className="text-gray-500">
-              <svg fill="currentColor" strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" className="w-5 h-5" viewBox="0 0 24 24">
-                <path d="M21 11.5a8.38 8.38 0 01-.9 3.8 8.5 8.5 0 01-7.6 4.7 8.38 8.38 0 01-3.8-.9L3 21l1.9-5.7a8.38 8.38 0 01-.9-3.8 8.5 8.5 0 014.7-7.6 8.38 8.38 0 013.8-.9h.5a8.48 8.48 0 018 8v.5z"></path>
-              </svg>
-            </a>
-          </span> */}
-						</div>
-						<p className="leading-relaxed">
-							Fam locavore kickstarter distillery. Mixtape chillwave tumeric sriracha taximy chia
-							microdosing tilde DIY. XOXO fam indxgo juiceramps cornhole raw denim forage brooklyn.
-							Everyday carry +1 seitan poutine tumeric. Gastropub blue bottle austin listicle pour-over,
-							neutra jean shorts keytar banjo tattooed umami cardigan.
-						</p>
+						<div className="mb-4 flex"></div>
+						<p className="leading-relaxed">{product.description}</p>
 						<div className="mb-5 mt-6 flex items-center border-b-2 border-gray-100 pb-5">
 							<div className="flex">
 								<span className="mr-3">Color</span>
@@ -105,12 +83,12 @@ const PageProduct = async ({ params: { slugId } }: { params: { slugId: string } 
 								</div>
 							</div>
 						</div>
-						<div className="flex">
+
+						<form className="flex" action={addProductToCartAction}>
 							<span className="title-font text-2xl font-medium text-gray-900">$58.00</span>
-							<button className="ml-auto flex rounded border-0 bg-indigo-500 px-6 py-2 text-white hover:bg-indigo-600 focus:outline-none">
-								Button
-							</button>
-							<button className="ml-4 inline-flex h-10 w-10 items-center justify-center rounded-full border-0 bg-gray-200 p-0 text-gray-500">
+							<AddCartButton />
+
+							{/* <button className="ml-4 inline-flex h-10 w-10 items-center justify-center rounded-full border-0 bg-gray-200 p-0 text-gray-500">
 								<svg
 									fill="currentColor"
 									strokeLinecap="round"
@@ -121,8 +99,8 @@ const PageProduct = async ({ params: { slugId } }: { params: { slugId: string } 
 								>
 									<path d="M20.84 4.61a5.5 5.5 0 00-7.78 0L12 5.67l-1.06-1.06a5.5 5.5 0 00-7.78 7.78l1.06 1.06L12 21.23l7.78-7.78 1.06-1.06a5.5 5.5 0 000-7.78z"></path>
 								</svg>
-							</button>
-						</div>
+							</button> */}
+						</form>
 					</div>
 				</div>
 			</div>
