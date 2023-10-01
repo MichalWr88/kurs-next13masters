b0e500e029ffe3e6c14e3fb431cf2ed790db9860
mmaleszewski <mmaleszewski@trans.eu>
2023-10-01 09:00:24 +0200

    chore :add removeProduct and revalidate cart Tag

--- src/app/(withNavBar)/cart/actions.ts
+++ src/app/(withNavBar)/cart/actions.ts
@@ -1,15 +1,29 @@
 "use server";
 
+import { revalidateTag } from "next/cache";
 import { executeGraphql } from "@/api/graphQL/graphQLProvider";
-import { CartSetProductQuantityDocument } from "@/gql/graphql";
+import { CartRemoveProductItemDocument, CartSetProductQuantityDocument } from "@/gql/graphql";
 
 export const changeItemQuantity = async (itemId: string, quantity: number, price: number) => {
 	const newTotal = Number(((price * 10 * (quantity * 10)) / (10 * 10)).toFixed(2));
-	const resp = await executeGraphql({query:CartSetProductQuantityDocument, variables: {
-		id: itemId.toString(),
-		quantity: quantity,
-		total: newTotal,
-	}});
-
+	const resp = await executeGraphql({
+		query: CartSetProductQuantityDocument,
+		variables: {
+			id: itemId.toString(),
+			quantity: quantity,
+			total: newTotal,
+		},
+	});
+	revalidateTag("cart");
 	return resp.updateOrderItem?.data;
+};
+export const removeProductItem = async (itemId: number) => {
+	const resp = await executeGraphql({
+		query: CartRemoveProductItemDocument,
+		variables: {
+			id: itemId.toString(),
+		},
+	});
+	revalidateTag("cart");
+	return resp.deleteOrderItem?.data?.id;
 };
