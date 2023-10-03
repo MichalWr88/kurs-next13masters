6f07b60f27a1d084b9f9eee2246a6831cfd9ba65
mmaleszewski <mmaleszewski@trans.eu>
2023-10-03 14:59:35 +0200

    chore: change tag for categories (p)

--- src/UI/Product/ProductElem.tsx
+++ src/UI/Product/ProductElem.tsx
@@ -16,14 +16,15 @@ const ProductElem = ({ product }: Props) => {
 			{images?.data[0] && <ProductImg product={product} />}
 			<figcaption className="mt-4">
 				<div className="flex gap-2">
+
 					{categories &&
 						categories.data?.map((cat) => (
-							<h3
+							<p
 								key={`categories-${cat.attributes?.slug}`}
 								className="mb-1 text-xs tracking-widest text-gray-500"
 							>
 								<AppLink route={`/categories/${cat.attributes?.slug}`}>{cat.attributes?.name}</AppLink>
-							</h3>
+							</p>
 						))}
 				</div>
 				<h2 className="text-lg font-medium text-gray-900">
