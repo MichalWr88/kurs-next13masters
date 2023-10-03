ccc4b5caf3b618c88dc40ac7f749289c4c7edd35
mmaleszewski <mmaleszewski@trans.eu>
2023-10-03 15:05:08 +0200

    chore: change ProductListHeader

--- src/UI/ProductList/ProductListHeader.tsx
+++ src/UI/ProductList/ProductListHeader.tsx
@@ -1,7 +1,7 @@
 
 const ProductListHeader = ({header}:{header:string}) => (
-    <header>
-    <h1 className="container  p-2 text-xl uppercase">{header}</h1>
+    <header className="container  p-2 text-xl uppercase">
+   {header}
   </header>
   )
 
