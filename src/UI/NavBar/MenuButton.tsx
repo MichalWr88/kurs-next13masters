c5e96b1b5348f2a4e0f505522c6a698538fa6100
mmaleszewski <mmaleszewski@trans.eu>
2023-11-06 14:03:06 +0100

    Refactor MenuButton component to use consistent formatting

--- src/UI/NavBar/MenuButton.tsx
+++ src/UI/NavBar/MenuButton.tsx
@@ -1,20 +1,15 @@
-
-import React from "react";
-
-
-
 const MenuButton = () => (
-		<button  className="block md:hidden">
-			<svg
-				xmlns="http://www.w3.org/2000/svg"
-				xmlSpace="preserve"
-				width="60"
-				height="60"
-				viewBox="0 0 24 24"
-			>
-				<path d="M20.25 12.75H3.75a.75.75 0 0 1 0-1.5h16.5a.75.75 0 0 1 0 1.5zM20.25 7H3.75a.75.75 0 0 1 0-1.5h16.5a.75.75 0 0 1 0 1.5zM20.25 18.5H3.75a.75.75 0 0 1 0-1.5h16.5a.75.75 0 0 1 0 1.5z" />
-			</svg>
-		</button>
-	);
+	<button className="block md:hidden">
+		<svg
+			xmlns="http://www.w3.org/2000/svg"
+			xmlSpace="preserve"
+			width="60"
+			height="60"
+			viewBox="0 0 24 24"
+		>
+			<path d="M20.25 12.75H3.75a.75.75 0 0 1 0-1.5h16.5a.75.75 0 0 1 0 1.5zM20.25 7H3.75a.75.75 0 0 1 0-1.5h16.5a.75.75 0 0 1 0 1.5zM20.25 18.5H3.75a.75.75 0 0 1 0-1.5h16.5a.75.75 0 0 1 0 1.5z" />
+		</svg>
+	</button>
+);
 
 export default MenuButton;
